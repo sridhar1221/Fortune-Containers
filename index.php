1f7dc42e9e6fd3ba15a265a7d87a101b55e95b66
sridhar1221 <162272117+sridhar1221@users.noreply.github.com>
2025-03-29 19:24:13 +0530

    add image

--- index.php
+++ index.php
@@ -73,8 +73,39 @@
     <div class="welcome-text">
         
         <h2><u>Welcome to Fortune Containers</u></h2>
-        <p>PET (Polyethylene Terephthalate) plastics are clear, durable, and resistant to heat, making them an excellent choice for product packaging. Available in a variety of shapes and colors, PET containers are widely used across industries.</p>
-        <p>Our PET containers provide UV protection for light-sensitive products and are highly resistant to acids, oils, and alcohols. Recyclable and eco-friendly, PET products contribute to sustainability by being repurposed into various other materials.</p>
+        <p>PET (Polyethylene Terephthalate) plastics are clear, durable, and resistant to heat, making them an excellent choice for product packaging. Available in a variety of shapes and colors, PET containers are widely used across industries.
+        Our PET containers provide UV protection for light-sensitive products and are highly resistant to acids, oils, and alcohols. Recyclable and eco-friendly, PET products contribute to sustainability by being repurposed into various other materials.</p>
+        <h2><u>Our Quality Assurance</u></h2>
+                
+                <p>
+                    We manufacture our entire range of products using excellent quality material. 
+                    Our products are widely appreciated and demanded across the globe. We have managed 
+                    to develop our own standards and measures to check quality of the product with help 
+                    of our own developed testing unit. And for this we have special team of quality experts 
+                    who test out raw material at different parameter from production to supply stage. Our 
+                    quality assurance and control procedures begin as soon as primary materials enter the factory. 
+                    Achieving end users satisfaction has always been our main motto.
+                </p>
+
+                <h2><u>Pet Bottle</u></h2>
+        
+        <p>
+            Our customers can avail from us a superior quality assortment of Pet Bottles. 
+            We offer these bottles in different sizes and capacities to meet the diverse 
+            requirements of our clients. Manufactured using high-grade raw material, these 
+            bottles are widely known for their quality and durability. These bottles are also 
+            tested on various parameters so as to ensure their adherence with set industry standards and norms.
+        </p>
+
+        
+            <h2><u>Pet Plastic Bottles</u></h2>
+        
+        <p>
+            Owing to industrial expertise, we are engaged in offering patrons with fine quality Pet Plastic Bottles. 
+            The offered bottles and jars are designed as per the prevailing industrial norms to ensure these are able 
+            to stand tall on the expectations of patrons. Moreover, we design these bottles and jars as per the specifications 
+            given by patrons by making use of high quality raw material to ensure their higher level of satisfaction.
+        </p>
     </div>
     <div class="footer">&copy; 2024 Fortune Containers. All Rights Reserved.<br>
         Designed by:<a href="https://inexpensivecoders.com/" class="a1">Inexpencive coders</a></div>
